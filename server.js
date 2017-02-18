fea9f1d291e90c1d2346746eb11df114b62aa08b
Matteo Merzi <matteo.merzi@gmail.com>
2017-02-18 15:41:35 +0100

    Added projects page

--- server.js
+++ server.js
@@ -47,13 +47,19 @@ app.get('/about', (req, res) => {
     res.render('about.hbs', {
         pageTitle: 'About Page'
     });
-})
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects page'
+    });
+});
 
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Unable to handle request'
     });
-})
+});
 
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
